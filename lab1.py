6d8d886308666b75824b971aa7593b0073876bfe
sensible-heart <alexandra.makepeace@gmail.com>
2016-09-07 15:18:14 -0600

    Added changes to lab1.py.

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 import requests
 
 #returns the status code response = requests.get('http://www.google.ca')
-response = requests.post('http://ccid-eddieantonio.rhcloud.com/makepeac')
-print response.status_code
+#response = requests.post('http://ccid-eddieantonio.rhcloud.com/makepeac')
+response = requests.get("https://raw.githubusercontent.com/sensible-heart/CMPUT404Lab1/master/lab1.py")
+print response.text
